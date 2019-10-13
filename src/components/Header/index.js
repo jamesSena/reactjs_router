e3dbffe1064df2a3d7a65a11c74ac4100bfe7694
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-10-12 23:13:35 -0300

    Configuração minima

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Header extends Component{
+  render(){
+    return (
+      <div className="Header-container">
+        Meu Header
+        <hr/>
+     </div>
+    );
+  }
+}
+export default Header;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Sobre extends Component{
+  render(){
+    return (
+      <div className="Sobre-container">
+        Sobre
+      </div>
+    );
+  }
+}
+export default Sobre;
