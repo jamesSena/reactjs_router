832770103742491c2fd762239d52e5f252f26b39
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-10-12 23:34:18 -0300

    adicionando a navegação no Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class Header extends Component{
   render(){
     return (
       <div className="Header-container">
-        Meu Header
+        Meu Header - 
+        <Link to="/">Ir para Home </Link>
+        -
+        <Link to="/sobre">Ir para Sobre </Link>
+
         <hr/>
      </div>
     );
