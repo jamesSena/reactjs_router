165623284719e7c4a826bee926ae745a9540a591
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-10-13 15:09:36 -0300

    Consultando uma api get, retornando json

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import './index.css';
 
 class Header extends Component{
   render(){
     return (
-      <div className="Header-container">
+      <div className="container">
         Meu Header - 
         <Link to="/">Ir para Home </Link>
         -
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,28 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
 class Home extends Component{
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      filmes:[]
+    }
+    this.loadFilmes = this.loadFilmes.bind(this);
+  }
+
+  componentDidMount(){
+    this.loadFilmes();
+  }
+loadFilmes(){
+  let url = 'https://sujeitoprogramador.com/r-api/?api=filmes';
+  fetch(url)
+  .then((r)=>r.json())
+  .then((json)=>{
+    this.setState({filmes:json})
+    console.log(this.state.filmes);
+  });
+
+}
   render(){
     return (
       <div className="Home-container">
